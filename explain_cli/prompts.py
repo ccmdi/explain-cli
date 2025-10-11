bcd5d04c7b73c3a4fb9da34d7b95dbff462a2d45
ccmdi <ccmdi.dev@gmail.com>
2025-10-11 10:25:43 -0400

    v

--- explain_cli/prompts.py
+++ explain_cli/prompts.py
@@ -1,4 +1,4 @@
-BP = """* All descriptions are for code review. Focus on specific changes and not broad intent.* NEVER mention the context of the description, e.g NEVER SAY "based on the diff...", "based on the commit...", "this diff...", etc. NEVER EVEN SAY THE WORD "DIFF". Always just describe. THAT'S IT."""
+BP = """* All descriptions are for code review. Focus on specific changes and not broad intent.* NEVER mention the context of the description, e.g NEVER SAY "based on the diff...", "based on the commit...", "this diff...", etc. NEVER EVEN SAY THE WORD "DIFF". Always just describe. THAT'S IT. * Try and stick to bullet points if there are more than a few changes."""
 
 def EXPLAIN_DIFF_BP(ref): 
     return BP + f"""Provide a summary of the changes between the current repository state and commit '{ref}'. Describe what has changed and the main differences. Here is the diff:"""
